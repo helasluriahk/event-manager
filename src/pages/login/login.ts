284440bc01b65ed533ea2505d0da272cb28343d3
unknown <helasluriahk@gmail.com>
2018-04-10 08:39:35 +0700

    commit this

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Alert, AlertController, Loading, LoadingController} from 'ionic-angular';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {HomePage} from '../home/home';
-import {AuthProvider} from '../../providers/auth/auth'
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  Alert, AlertController,
+  Loading, LoadingController
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HomePage } from '../../pages/home/home';
+import { AuthProvider } from '../../providers/auth/auth';
 
 @IonicPage()
 @Component({
@@ -18,42 +14,64 @@ import {AuthProvider} from '../../providers/auth/auth'
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  public loginform: FormGroup;
+  public loginForm: FormGroup;
   public loading: Loading;
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams, 
-              public alertctrl: AlertController, 
-              public loadingctrl: LoadingController,
-              public authprovider: AuthProvider,
-              public formbuilder: FormBuilder) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    public formBuilder: FormBuilder) {
+
+    // validasi form
+    this.loginForm = formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      ]
+    });
+
   }
 
-  //proses login user
-  loginuser(){
-    const email = this.loginform.value.email;
-    const pass = this.loginform.value.pass;
+  // proses login user
+  loginUser() {
+    // cek apakah email dan password sudah valid
+    if (!this.loginForm.valid) {
+      console.log(`Form tidak valid: ${this.loginForm.value}`);
+    } else {
+      // baca formControlName dahulu
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
 
-    this.authprovider.loginuser(email, pass).then(authdata=>{
-      this.loading.dismiss().then(()=> {
-        this.navCtrl.setRoot(HomePage);
-      });
-    },
-    error => {
-      this.loading.dismiss().then(()=>{
-        const alert: Alert = this.alertctrl.create({
-          message: error.message,
-          buttons: [{
-            text:'OK',
-            role:'Cancel'
-          }]
-        });
-        alert.present();
-      });
+      // cek dari firebase
+      this.authProvider.loginUser(email, password).then(
+        authData => {       // resolve
+          this.loading.dismiss().then(() => {
+            this.navCtrl.setRoot(HomePage);
+          });
+        },
+        error => {          // reject
+          this.loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{
+                text: 'Ok',
+                role: 'cancel'
+              }]
+            });
+            alert.present();
+          });
+        }
+      );
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
     }
-  );
-  this.loading = this.loadingctrl.create();
-  this.loading.present();
   }
 
   ionViewDidLoad() {
